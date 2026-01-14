e24ad8519938aedd7f4a78067153935f79e62a78
Runoah <rubengarciacubero@gmail.com>
2026-01-14 17:26:33 +0100

    Update REACT

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,32 @@
-export default function App() {
-return <ParentComponent />
-}
-function ParentComponent() {
+function App() {
+// comentario
 return (
 <>
-<UserComponent />
-<ProfileComponent />
-<FeedComponent />
+<h1>Componentes en marcha</h1>
+<h2>Fragmentos y composición</h2>
+{/* Comentario dentro del jsx? */}
+{/* Comentario añadido con CTRL K + CTRL C */}
+<ParentComponent />
 </>
 )
+
+}
+export default App
+
+function ParentComponent() {
+    return (
+    <>
+    <UserComponent />
+    <ProfileComponent />
+    <FeedComponent />
+    </>
+    )
 }
 function UserComponent() {
-return <h2>User component</h2>
+    return <h2>User component</h2>
 }
 function ProfileComponent() {
-return <h2>Profile component</h2>
+    return <h2>Profile component</h2>
 }
 function FeedComponent() {
 return <h2>Feed component</h2>
