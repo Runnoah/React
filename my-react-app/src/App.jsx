0d84308ea3d9124015638866579fd915041eb17b
Runoah <rubengarciacubero@gmail.com>
2026-01-15 16:04:13 +0100

    Actualización

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,38 @@
-function App() {
-// comentario
-return (
-<>
-<h1>Componentes en marcha</h1>
-<h2>Fragmentos y composición</h2>
-{/* Comentario dentro del jsx? */}
-{/* Comentario añadido con CTRL K + CTRL C */}
-<ParentComponent />
-</>
-)
+function App() { 
 
-}
-export default App
-
-function ParentComponent() {
+    const users = [
+        { id: 1, name: 'Jose', role: 'Web Developer' },
+        { id: 2, name: 'Estefanía', role: 'Web Designer' },
+        { id: 3, name: 'Rubén', role: 'Team Leader' },
+        { id: 4, name: 'Klara', role: 'Project Manager' },
+        { id: 5, name: 'Miguel', role: 'Backend Developer' }
+    ]
+    
+    const techClass = 'tec-estilo';
+    const managementClass = 'equipo-estilo';
+    
     return (
-    <>
-    <UserComponent />
-    <ProfileComponent />
-    <FeedComponent />
-    </>
+        <>
+        <p>Lista de usuarios activos:</p>
+        <ul>
+        {users.map(function(user) {
+            let estiloAplicado;
+
+          if (user.role.includes('Developer') || user.role.includes('Designer')) {
+            estiloAplicado = techClass;
+          } else {
+            estiloAplicado = managementClass;
+          }            
+          
+          return (
+                <li key={user.id} className={estiloAplicado}>
+                    {user.name} - {user.role}
+                </li>
+                );
+        })}
+        </ul>
+        </>
     )
 }
-function UserComponent() {
-    return <h2>User component</h2>
-}
-function ProfileComponent() {
-    return <h2>Profile component</h2>
-}
-function FeedComponent() {
-return <h2>Feed component</h2>
-}
+
+export default App
