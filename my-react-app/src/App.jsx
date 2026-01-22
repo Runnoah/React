a115321971511c14a4c2eff9ae85dc8da6456405
Runoah <rubengarciacubero@gmail.com>
2026-01-22 16:48:59 +0100

    Miniretillo (tarjeta de verdad)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,26 +1,15 @@
+import './style.css';
 
 function App() {
-    return (
-        <>
-            <h1 className="titulo">Carta del Miniretillo</h1>
-            
-            <p className="descripcion">
-                Esto es una descripción para la carta, algo asi, cortita, 
-                vamos a añadirle un proverbio chino: Si me engañas una vez 
-                la culpa es tuya, si me engañas dos veces la culpa es mía.
-            </p>
-    
-            <br/>
-            <div className="centrado">
-
-            <button className="claseBoton">
-            Botón de la carta
-            </button>
-        
-            </div>
-        </>
-    );
+  return (
+    <div className="card">
+      <h1 className="titulo">Sabiduría</h1>
+      <p className="descripcion">
+        Proverbio chino: Si me engañas una vez la culpa es tuya, si me engañas dos veces la culpa es mía.
+      </p>
+      <button className="claseBoton">Aceptar</button>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
