c99ce460d52899ca7046c60ecce16b1ff1b9620a
Runoah <rubengarciacubero@gmail.com>
2026-01-22 17:06:42 +0100

    Parte C (7)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,41 @@
-import './style.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="card">
-      <h1 className="titulo">Sabiduría</h1>
-      <p className="descripcion">
-        Proverbio chino: Si me engañas una vez la culpa es tuya, si me engañas dos veces la culpa es mía.
-      </p>
-      <button className="claseBoton">Aceptar</button>
-    </div>
-  );
+    const [username, setUsername] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+
+    const handleUsername = (e) => {
+        const { value } = e.target;
+        setUsername(value);
+
+        if (value.length > 0 && value.length <= 6) {
+            setUsernameError('El username debe tener más de 6 caracteres');
+        } else {
+            setUsernameError('');
+        }
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (usernameError || username === '') {
+            alert('No se puede enviar: el formulario contiene errores');
+        } else {
+            alert(username);
+        }
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={username} 
+                    onChange={handleUsername} 
+                />
+                <button type="submit">Enviar</button>
+            </form>
+            {usernameError && <p>{usernameError}</p>}
+        </>
+    );
 }
 
 export default App;
